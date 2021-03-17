448bc0c0d859e6f47333fd86b3d4992836d70378
Eduardof0nt <eduardofontc@hotmail.com>
2021-03-16 18:38:58 -0600

    Tarea 2 1.3

--- Ejercicio 1.py
+++ Ejercicio 1.py
@@ -1,35 +1,36 @@
 import matplotlib.pyplot as plt
 import numpy as np
 
-#fp is the function f(y,t) that represents the derivative of the differential equation, h is the step size, n is the number of steps, x0 is the strat value of the independent variable and y0 is the value of the dependent variable at x0.
+#fp es la función f(y,t) que representa la derivada de la función que se desea encontrar en la equación diferencial, h es el tamaño de los pasos, n es el número de pasos, x0 es el valor inicial de la variable independiente and y0 el valor de la variable dependiente (la función que se desea encontrar) en x0.
 def RungeKutta4(fp, h, n, x0, y0):
-    results = np.array([[x0],[y0]]) #Initializes an array to hold the aproximated points of the solution.
-    for i in range(0, n): #Iterates through the commanded steps to find each point of the aproximated solution.
-        #Calculate each k factor using the previous result stored in the results array.
+    results = np.array([[x0],[y0]]) #Se inicia un arreglo para guardar los puntos calculados de la solución aproximada.
+    for i in range(0, n): #Se itera sobre los pasos deseados para calcular en cada uno los puntos aproximados de la solución.
+        #Se calcula cada una de las k con los valores del punto calculado en el paso anterior.
         k1 = fp(results[0, i], results[1, i])
         k2 = fp(results[0, i] + h/2, results[1, i] + h*k1/2)
         k3 = fp(results[0, i] + h/2, results[1, i] + h*k2/2)
         k4 = fp(results[0, i] + h, results[1, i] + h*k3)
-        #Finally the next result is calculated and the x and y values are added to the array. the axis=1 statement makes the new values be appended to the array as a new column (because the top row are the x's and the bottom the y's).
+        #Finalmente, se calcula el nuevo punto de la solución y se agrega al arreglo. El parámetro axis=1 hace que el nuevo punto se concatene como una columna. De esta forma, la fila de arriba son los valores de la variable independiente y los de la fila de abajo los de la variable dependiente.
         results = np.append(results,[[results[0, i] + h], [results[1, i] + (h/6)*(k1 + 2*k2 + 2*k3 + k4)]], axis=1)
-    return results #Returns the complete array of aproximated solutions.
+    return results #Retorna el arreglo con todos los puntos calculados.
 
-#Define the function to aproximate.
+#Se define la función que es igual al valor de la derivada de la función que se desea encontrar.
 def f(y,P):
-    g = 9.8 #The gravity.
-    M = 0.0289647 #Molar mass in kg/mol, this is done for matching units in the funciton, given value is 28.9647 g/mol.
-    R = 8.314462  #The gas constant.
-    return -g*M*P/(R*(293-y/200)) #The result of the function, this equals dP/dy in the given point.
+    g = 9.8 #La gravedad.
+    M = 0.0289647 #La masa molar en kg/mol, esto se hace para que las unidades calcen, ya que el valor dado está en g/mol.
+    R = 8.314462  #La constante de gas.
+    return -g*M*P/(R*(293-y/200)) #El resultado de la función, esto es igual a dP/dy en el punto dado.
 
 
-#Aproximation parameters.
-h = 100 #Step size 100 m.
-n = 30 #30 steps.
-y0 = 0 #Initial elevation of 0 m.
-p0 = 101325 #Initial preassure of 101 325 Pa.
+#Parámetros para la aproximación.
+h = 100 #Tamaño de paso de 100 m.
+n = 30 #30 pasos.
+y0 = 0 #Elevación inicial de 0 m.
+p0 = 101325 #Presión inicial de 101 325 Pa.
 
-#The results are calculated and plotted.
+#Se calculan los resultados y se grafican.
 results = RungeKutta4(f, h, n, y0, p0)
+
 plt.plot(results[0,:], results[1,:])
 plt.title("P(Pa) vs. y(m)")
 plt.xlabel("y(m)")
